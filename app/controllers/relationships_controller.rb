40b8a7e6f1ccba05c984127af65a5ed86d620793
EC2 Default User <ec2-user@ip-172-31-29-216.ap-northeast-1.compute.internal>
2022-08-23 06:09:50 +0000

    応用課題４終了

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   
   def create
     current_user.follow(params[:user_id])
-    redirect_to reqoest.referer
+    redirect_to request.referer
   end
   
   def destroy
