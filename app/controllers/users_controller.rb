db962f46e115ad49340c00773d032ef9c43e76bc
EC2 Default User <ec2-user@ip-172-31-29-216.ap-northeast-1.compute.internal>
2022-09-05 02:28:49 +0000

    応用課題８

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+        if @isRoom
+        else
+         @room = Room.new
+         @entry = Entry.new
+        end
+    end
+
     @books = @user.books
     @book = Book.new
   end
@@ -10,7 +28,7 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
    if @user == current_user
@@ -27,7 +45,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  
+
+
   has_many :books
   has_many :book_comments,dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-    
+
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
   has_one_attached :profile_image
- 
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(user)
     relationships.create(followed_id: user.id)
   end
@@ -27,15 +30,15 @@ class User < ApplicationRecord
   def unfollow(user)
     relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
